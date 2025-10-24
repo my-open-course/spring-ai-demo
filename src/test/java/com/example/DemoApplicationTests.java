238b635a65c22cb2e30282a30b5d692f65adcf0b
刘德财 <442823529@qq.com>
2025-10-24 13:27:30 +0800

    API test

--- src/test/java/com/example/DemoApplicationTests.java
+++ src/test/java/com/example/DemoApplicationTests.java
@@ -1,14 +1,40 @@
 package com.example;
 
+import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
+    @Resource
+    private MockMvc mockMvc;
 
-	}
+    @Test
+    void contextLoads() {
 
+    }
+
+    @Test
+    void helloApiTest() throws Exception {
+        mockMvc.perform(get("/api/hello"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World"));
+    }
+
+    @Test
+    void chatApiTest() throws Exception {
+        mockMvc.perform(get("/api/chat"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
 }
